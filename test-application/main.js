e6ce5fb7ff26d98bbea25f5438c36980fa9b42c1
Gabriel Goller <gabrielgoller123@gmail.com>
2021-12-07 21:50:33 +0100

    :memo: Added documentation working fix docs

--- test-application/main.js
+++ test-application/main.js
@@ -2,7 +2,7 @@ const { Kafka } = require('kafkajs');
  
 const kafka = new Kafka({
   clientId: 'my-app',
-  brokers: ['kafka1:9092', 'kafka2:9092']
+  brokers: ['broker:9092']
 })
  
 const producer = kafka.producer()
